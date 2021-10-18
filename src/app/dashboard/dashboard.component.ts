bae0e4b894278e56b830000010cba287a5cad5f1
Julio César Juárez Martínez <julio.juarez@nexta.biz>
2021-10-18 18:37:31 -0500

    Cambios sobre iconos

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,19 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-dashboard',
 	templateUrl: './dashboard.component.html',
 	styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements AfterViewInit {
+export class DashboardComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+	firstFormGroup: FormGroup=Object.create(null);
+  toppings = new FormControl();
 
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 	ngAfterViewInit() { }
 
 }
