e462baedf11f251015a9d27d16e1f5743f8e4c2b
Julio César Juárez Martínez <julio.juarez@nexta.biz>
2021-10-19 10:51:25 -0500

    Cambios

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component,  OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ENTER, COMMA } from '@angular/cdk/keycodes';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 
 @Component({
 	selector: 'app-dashboard',
@@ -7,6 +9,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 	styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  panelOpenState = false;
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
@@ -15,5 +31,37 @@ export class DashboardComponent implements OnInit {
 
   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 	ngAfterViewInit() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
 
+
+  // Enter, comma
+  separatorKeysCodes = [ENTER, COMMA];
+
+  fruits = [{ name: 'Iztapalapa' }, { name: 'Izatacalco' }, { name: 'Condesa' }];
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(fruit: any): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
 }
--- src/app/material-component/grid/grid.component.ts
+++ src/app/material-component/grid/grid.component.ts
@@ -7,30 +7,73 @@ import {MatGridListModule} from '@angular/material/grid-list';
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent {
-  tiles = [
+  cards = [
+    {title: 'Title 1', content: 'Content 1'},
+    {title: 'Title 2', content: 'Content 2'},
+    {title: 'Title 3', content: 'Content 3'},
+    {title: 'Title 4', content: 'Content 4'}
+  ];
+  typesOfShoes = [
+    'Administracion de medicamentos via oral.',
+    'Toma de signos vitales y vigilancia de los mismos',
+    'Procedimientos médicos básicos (inyecciones, suero, etc)',
+    'Ayuda en higiene personal (baño de esponja, tina, etc)',
+    'Administración de fórmula y otros alimentos',
+    'Limpieza y esterilización de botellas y chupones',
+    'Ayuda en tareas escolares',
+    'Implementación  de actividades recreativas.',
+    'Acompañamiento a clases particulares o  actividades extracurriculares.'
+  ];
+  typesOfShoes2 = [
+    'Toma de signos vitales y vigilancia de los mismos',
+    'Procedimientos médicos básicos (inyecciones, suero, etc)',
+    'Administración de medicamentos vía oral',
+    'Ayuda en higiene personal (baño de esponja, tina, etc)',
+    'Acompañante en vistas médicas, paseos y actividades recreativas'
+  ];
+  messages = [
     {
-      text: 'One',
-      cols: 3,
-      rows: 1,
-      color: 'lightblue'
+      from: 'Nirav joshi (nbj@gmail.com)',
+      image: 'assets/images/users/1.jpg',
+      subject: 'Material angular',
+      content: 'This is the material angular template'
     },
     {
-      text: 'Two',
-      cols: 1,
-      rows: 2,
-      color: 'lightgreen'
+      from: 'Sunil joshi (sbj@gmail.com)',
+      image: 'assets/images/users/2.jpg',
+      subject: 'Wrappixel',
+      content: 'We have wrappixel launched'
     },
     {
-      text: 'Three',
-      cols: 1,
-      rows: 1,
-      color: 'lightpink'
+      from: 'Vishal Bhatt (bht@gmail.com)',
+      image: 'assets/images/users/3.jpg',
+      subject: 'Task list',
+      content: 'This is the latest task hasbeen done'
+    }
+  ];
+
+  folders = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16')
     },
     {
-      text: 'Four',
-      cols: 2,
-      rows: 1,
-      color: '#DDBDF1'
+      name: 'Recipes',
+      updated: new Date('1/17/16')
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16')
+    }
+  ];
+  notes = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16')
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16')
     }
   ];
 }
