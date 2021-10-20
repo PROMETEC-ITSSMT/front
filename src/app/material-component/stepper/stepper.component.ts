dd1b7ce5e884f5a40337179552b23997128536b1
Julio Juarez <juarezmartinez.juliocesar@gmail.com>
2021-10-20 11:04:17 -0500

    asiganecion de tabla

--- src/app/material-component/stepper/stepper.component.ts
+++ src/app/material-component/stepper/stepper.component.ts
@@ -3,7 +3,38 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 import { MatStepperModule } from '@angular/material/stepper';
 
+
+export interface PeriodicElement {
+	duracion: number;
+	descripcion: number;
+	tSugerida: number;
+	tRecibida: number;
+  }
+
+  const ELEMENT_DATA: PeriodicElement[] = [
+
+	{descripcion: 1, duracion: 1, tSugerida: 104.40, tRecibida: (108.80*1)},
+	{descripcion: 2, duracion: 2, tSugerida: (104.40*2), tRecibida: (108.80*2)},
+	{descripcion: 3, duracion: 3, tSugerida: (104.40*3), tRecibida: (108.80*3)},
+	{descripcion: 4, duracion: 4, tSugerida: (104.40*4), tRecibida: (108.80*4)},
+	{descripcion: 5, duracion: 5, tSugerida: (104.40*5), tRecibida: (108.80*5)},
+	{descripcion: 6, duracion: 6, tSugerida: (104.40*6), tRecibida: (108.80*6)},
+	{descripcion: 7, duracion: 7, tSugerida: (104.40*7), tRecibida: (108.80*7)},
+	{descripcion: 8, duracion: 8, tSugerida: (104.40*8), tRecibida: (108.80*8)},
+	{descripcion: 9, duracion: 9, tSugerida: (104.40*9), tRecibida: (108.80*9)},
+	{descripcion: 10, duracion: 10, tSugerida: (104.40*10), tRecibida: (108.80*10)},
+	{descripcion: 11, duracion: 11, tSugerida: (104.40*11), tRecibida: (108.80*11)},
+	{descripcion: 12, duracion: 12, tSugerida: (104.40*12), tRecibida: (108.80*12)},
+	{descripcion: 13, duracion: 13, tSugerida: (104.40*13), tRecibida: (108.80*13)},
+	{descripcion: 14, duracion: 14, tSugerida: (104.40*14), tRecibida: (108.80*14)},
+
+
+  ];
+
+
+
 @Component({
+
 	selector: 'app-stepper',
 	templateUrl: './stepper.component.html',
 	styleUrls: ['./stepper.component.scss'],
@@ -11,7 +42,13 @@ import { MatStepperModule } from '@angular/material/stepper';
 	    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
 	}]
 })
+
 export class StepperComponent implements OnInit {
+
+	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+	dataSource = ELEMENT_DATA;
+	clickedRows = new Set<PeriodicElement>();
+ 
 	isLinear = false;
 	firstFormGroup: FormGroup=Object.create(null);
 	secondFormGroup: FormGroup=Object.create(null);
@@ -98,3 +135,4 @@ export class StepperComponent implements OnInit {
 		});
 	}
 }
+
